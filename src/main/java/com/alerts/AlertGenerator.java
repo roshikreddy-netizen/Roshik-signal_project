95cc00f4a2204057d762725f8b969f3f15937b81
Roshik Pakhala <r.pakhala@student.maastrichtuniversity.nl>
2026-04-20 06:46:07 +0200

    commit

--- src/main/java/com/alerts/AlertGenerator.java
+++ src/main/java/com/alerts/AlertGenerator.java
@@ -4,17 +4,17 @@ import com.data_management.DataStorage;
 import com.data_management.Patient;
 
 /**
- * The {@code AlertGenerator} class is responsible for monitoring patient data
+ * The class is responsible for monitoring patient data
  * and generating alerts when certain predefined conditions are met. This class
- * relies on a {@link DataStorage} instance to access patient data and evaluate
+ * relies on a instance to access patient data and evaluate
  * it against specific health criteria.
  */
 public class AlertGenerator {
     private DataStorage dataStorage;
 
     /**
-     * Constructs an {@code AlertGenerator} with a specified {@code DataStorage}.
-     * The {@code DataStorage} is used to retrieve patient data that this class
+     * Constructs an  with a specified {@code DataStorage}.
+     * The  is used to retrieve patient data that this class
      * will monitor and evaluate.
      *
      * @param dataStorage the data storage system that provides access to patient
@@ -27,7 +27,7 @@ public class AlertGenerator {
     /**
      * Evaluates the specified patient's data to determine if any alert conditions
      * are met. If a condition is met, an alert is triggered via the
-     * {@link #triggerAlert}
+     * 
      * method. This method should define the specific conditions under which an
      * alert
      * will be triggered.
